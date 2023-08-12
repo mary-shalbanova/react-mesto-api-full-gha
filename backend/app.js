70763a1bc79dfc39aaae1da7f664b35c874560f3
Maria Shalbanova <mariashalbanova@yandex.ru>
2023-08-12 18:23:07 +0300

    initial commit

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT, DB_URL } = process.env;
+const { PORT = 4000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const routes = require('./routes');
 const errorHandler = require('./middlewares/error-handler');
 
@@ -19,7 +19,6 @@ mongoose.connect(DB_URL, {
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
-
 });
 
 app.use(helmet());
